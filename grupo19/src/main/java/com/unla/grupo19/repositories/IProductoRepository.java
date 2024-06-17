a43421cee7288ee58dcf843cd0bd63ca85aaa490
ulises-justo-saucedo <ulisesjustosaucedo@gmail.com>
2024-06-17 18:03:58 -0300

    Corrección sobre el repositorio de productos

--- grupo19/src/main/java/com/unla/grupo19/repositories/IProductoRepository.java
+++ grupo19/src/main/java/com/unla/grupo19/repositories/IProductoRepository.java
@@ -2,9 +2,11 @@ package com.unla.grupo19.repositories;
 
 import com.unla.grupo19.entities.Producto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.io.Serializable;
 
+@Repository
 public interface IProductoRepository extends JpaRepository<Producto, Serializable> {
-
+    public Producto findByCodigo(String codigo);
 }
